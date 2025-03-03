7a19b12cf14c982f7e5be6cf82db51e985cc934c
Akhil Menon <iammudumenon@gmail.com>
2025-03-03 18:56:40 +0530

    Changed Backend api

--- Backend/main.go
+++ Backend/main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
-	router.LoadHTMLGlob("Pages/*")
 
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(200, "index.html", gin.H{
-			"title": "Summer School",
+	router.GET("/api/hello", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello from the Go backend!",
 		})
 	})
 
 	router.Run(":8000")
-
 }
